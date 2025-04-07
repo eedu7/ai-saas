689288ca8f0625af4a4238916939a6c098888ad7
eedu7 <mueedahmad067@gmail.com>
2025-04-07 13:39:37 +0500

    Added the favicon

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 function LandingPage() {
     return (
-        <div>LandingPage</div>
+        <div>
+            Landing Page (Unprotected)
+            <div>
+                <Button asChild>
+                    <SignInButton />
+                </Button>
+                <Button asChild>
+                    <SignUpButton />
+                </Button>
+            </div>
+        </div>
     );
 }
 

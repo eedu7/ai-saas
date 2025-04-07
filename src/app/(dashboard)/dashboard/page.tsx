d09d48368ea8e860e6083cbba1d019950034337c
eedu7 <mueedahmad067@gmail.com>
2025-04-07 16:29:07 +0500

    Populate the tools of the dashboard with tools

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { ArrowRightIcon, LucideIcon, MessageSquareIcon } from "lucide-react";
+import { ArrowRightIcon, CodeIcon, ImageIcon, LucideIcon, MessageSquareIcon, MusicIcon, VideoIcon } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
@@ -12,6 +12,34 @@ const tools: { label: string; icon: LucideIcon; color: string; bgColor: string; 
         color: "text-violet-500",
         bgColor: "bg-violet-500/10",
         href: "/conversation",
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
+        href: "/music",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        color: "text-pink-700",
+        bgColor: "bg-pink-700/10",
+        href: "/image",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        color: "text-orange-700",
+        bgColor: "bg-orange-700/10",
+        href: "/video",
+    },
+    {
+        label: "Code Generation",
+        icon: CodeIcon,
+        color: "text-green-500",
+        bgColor: "bg-green-500/10",
+        href: "/code",
     },
 ];
 

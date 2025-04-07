f9ae0c73d266e38b42927534e09222af5717c75b
eedu7 <mueedahmad067@gmail.com>
2025-04-07 16:26:26 +0500

    Added the card to render the different services in the dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,45 @@
+"use client";
+
 import React from "react";
+import { ArrowRightIcon, LucideIcon, MessageSquareIcon } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+const tools: { label: string; icon: LucideIcon; color: string; bgColor: string; href: string }[] = [
+    {
+        label: "Conversation",
+        icon: MessageSquareIcon,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/conversation",
+    },
+];
 
 export default function DashboardPage() {
-    return <div></div>;
+    return (
+        <div>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-center text-2xl font-bold md:text-4xl">Explore the power of AI</h2>
+                <p className="text-muted-foreground text-center text-sm font-light md:text-lg">
+                    Chat with the smartest AI - Experience the power of AI
+                </p>
+            </div>
+            <nav className="space-y-4 px-4 md:px-20 lg:px-32">
+                {tools.map(({ label, icon: Icon, color, bgColor, href }) => (
+                    <Card
+                        key={href}
+                        className="flex cursor-pointer flex-row items-center justify-between border-black/5 p-4 transition hover:shadow-md"
+                    >
+                        <div className="flex items-center gap-x-4">
+                            <div className={cn("w-fit rounded-md p-2", bgColor)}>
+                                <Icon className={cn("size-8", color)} />
+                            </div>
+                            <div className="font-semibold">{label}</div>
+                        </div>
+                        <ArrowRightIcon className="size-5" />
+                    </Card>
+                ))}
+            </nav>
+        </div>
+    );
 }

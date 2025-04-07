647f0ecb56827efd93b660e55d71bff56b47bba9
eedu7 <mueedahmad067@gmail.com>
2025-04-07 16:03:57 +0500

    Commented the code, if the hydration occurs, then uncomment that, also modified the document-side bar, add dynamic class names'

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,17 @@ import { Navbar } from "@/components/navbar";
 import { DocumentSidebar } from "@/components/document-sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    // NOTE: A simple way to fix hydration error
+    // const [isMounted, setIsMounted] = React.useState<boolean>(false);
+    //
+    // useEffect(() => {
+    //     setIsMounted(true);
+    // }, []);
+    //
+    // if (!isMounted) {
+    //     return null;
+    // }
+
     return (
         <div className="relative h-full">
             <div className="z-[80] hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
--- src/components/document-sidebar.tsx
+++ src/components/document-sidebar.tsx
@@ -17,6 +17,7 @@ import {
     SettingsIcon,
     VideoIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -68,6 +69,8 @@ const routes: { label: string; icon: LucideIcon; href: string; color?: string }[
 ];
 
 export const DocumentSidebar = () => {
+    const pathName = usePathname();
+
     return (
         <aside className="flex h-full flex-col space-y-4 bg-[#111827] py-4 text-white">
             <div className="flex-1 px-3 py-2">
@@ -89,7 +92,11 @@ export const DocumentSidebar = () => {
                         <Link
                             key={href}
                             href={href}
-                            className="group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white"
+                            className={cn(
+                                "group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm" +
+                                    " font-medium transition hover:bg-white/10 hover:text-white",
+                                pathName === href ? "bg-white/10 text-white" : "text-zinc-400",
+                            )}
                         >
                             <div className="flex flex-1 items-center">
                                 <Icon className={cn("mr-3 size-5", color)} />

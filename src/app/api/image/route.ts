1005dae603098d9519d3bda76eeb0dc22a02a34c
eedu7 <mueedahmad067@gmail.com>
2025-04-08 10:21:32 +0500

    created the image page, and made some changes

---
+++ src/app/api/image/route.ts
@@ -0,0 +1,44 @@
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+// NOTE: This is for connecting with Google GenAI using OpenAI
+const API_KEY = process.env.GOOGLE_API_KEY!;
+const openAi = new OpenAI({
+    apiKey: API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+});
+
+// TODO: Properly assign role
+const instructionsMessage = {
+    role: "system",
+    content:
+        "You are a highly skilled Senior Full Stack Developer with 10+ years of experience in building scalable web applications. Your job is to generate clean, production-ready code for both frontend and backend tasks using modern technologies. You understand best practices, write well-structured code, and include helpful comments where necessary. Please follow the user's instructions carefully and output only the requested code",
+};
+
+export async function POST(req: Request) {
+    try {
+        const { userId } = await auth();
+
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+        const body = await req.json();
+        const { messages } = body;
+
+        if (!API_KEY) return new NextResponse("API key not configured", { status: 500 });
+
+        if (!messages) return new NextResponse("Messages are required", { status: 400 });
+
+        const response = await openAi.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [instructionsMessage, ...messages],
+        });
+
+        return NextResponse.json(response.choices[0].message);
+    } catch (error) {
+        console.log("[CODE_ERROR]", error);
+        return new NextResponse("Internal error", {
+            status: 500,
+        });
+    }
+}

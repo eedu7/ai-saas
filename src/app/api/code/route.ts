3749094d6736c2e5ee7bf1f79721d66f6d7d1770
eedu7 <mueedahmad067@gmail.com>
2025-04-08 08:49:59 +0500

    create /api/code endpoint and assign a system role

---
+++ src/app/api/code/route.ts
@@ -0,0 +1,45 @@
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+// NOTE: This is for connecting with Google GenAI using OpenAI
+const API_KEY = process.env.GOOGLE_API_KEY!;
+const openAi = new OpenAI({
+    apiKey: API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+});
+
+// TODO: Properly assign role
+const instructionsMessage = {
+    role: "system",
+    content:
+        "You are a code generator. You must answer only in markdown code snippets. Use code comments for" +
+        " explanation.",
+};
+
+export async function POST(req: Request) {
+    try {
+        const { userId } = await auth();
+
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+        const body = await req.json();
+        const { messages } = body;
+
+        if (!API_KEY) return new NextResponse("API key not configured", { status: 500 });
+
+        if (!messages) return new NextResponse("Messages are required", { status: 400 });
+
+        const response = await openAi.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [instructionsMessage, ...messages],
+        });
+
+        return NextResponse.json(response.choices[0].message);
+    } catch (error) {
+        console.log("[CODE_ERROR]", error);
+        return new NextResponse("Internal error", {
+            status: 500,
+        });
+    }
+}

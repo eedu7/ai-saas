35e110740a127f23bcdd0314eead6bc404c42e4d
eedu7 <mueedahmad067@gmail.com>
2025-04-08 23:01:20 +0500

    Created the connection with stripe and also made the subscriptino model

--- src/lib/api-limit.ts
+++ src/lib/api-limit.ts
@@ -1,28 +1,26 @@
 import { auth } from "@clerk/nextjs/server";
 import { MAX_FREE_COUNTS } from "@/constants";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prismaDb } from "@/lib/prisma-client";
 
 export const increaseApiLimit = async () => {
     const { userId } = await auth();
 
     if (!userId) return;
 
-    const userApiLimit = await prisma.userApiLimit.findUnique({
+    const userApiLimit = await prismaDb.userApiLimit.findUnique({
         where: {
             userId,
         },
     });
 
     if (userApiLimit) {
-        await prisma.userApiLimit.update({
+        await prismaDb.userApiLimit.update({
             where: { userId },
             // @ts-ignore
             data: { count: userApiLimit.count + 1 },
         });
     } else {
-        await prisma.userApiLimit.create({
+        await prismaDb.userApiLimit.create({
             data: { userId: userId, count: 1 },
         });
     }
@@ -33,7 +31,7 @@ export const checkApiLimit = async () => {
 
     if (!userId) return false;
 
-    const userApiLimit = await prisma.userApiLimit.findUnique({
+    const userApiLimit = await prismaDb.userApiLimit.findUnique({
         where: {
             userId,
         },
@@ -47,7 +45,7 @@ export const getApiLimitCount = async () => {
 
     if (!userId) return 0;
 
-    const userApiLimit = await prisma.userApiLimit.findUnique({
+    const userApiLimit = await prismaDb.userApiLimit.findUnique({
         where: {
             userId,
         },

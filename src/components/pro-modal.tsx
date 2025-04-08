34fa3dc5d94e97745c8b3447394962b665b8335d
eedu7 <mueedahmad067@gmail.com>
2025-04-08 21:21:15 +0500

    beautify the badge

--- src/components/pro-modal.tsx
+++ src/components/pro-modal.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useProModel } from "@/hooks/use-pro-model";
+import { Badge } from "@/components/ui/badge";
 
 export const ProModal = () => {
     const { isOpen, onClose } = useProModel();
@@ -14,7 +15,15 @@ export const ProModal = () => {
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle className="flex flex-col items-center justify-center gap-y-4 pb-2">
-                        Upgrade to AI SaaS
+                        <div className="flex items-center gap-x-2 py-1 font-bold">
+                            Upgrade to AI SaaS
+                            <Badge
+                                variant="premium"
+                                className="py-1 text-sm uppercase"
+                            >
+                                pro
+                            </Badge>
+                        </div>
                     </DialogTitle>
                 </DialogHeader>
             </DialogContent>
